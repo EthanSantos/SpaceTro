ac54effe86f63082df7c1ecfe69e2300fcf677c1
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-05-25 19:57:02 -0700

    database setup

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from supabase import create_client
-from dotenv import load_dotenv
-import os
+from supabase import create_client, Client
 
 app = Flask(__name__)
 
-# Initialize Supabase client
-supabase_url = "https://ayyogzlsqqbwocmuuwph.supabase.co"
-supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5eW9nemxzcXFid29jbXV1d3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MjA5NTEsImV4cCI6MjAzMjE5Njk1MX0.9y9t591c8v1RNCssBa4-GNK9ZQNpR7CDqVJfvycnWCk"
-supabase = create_client(supabase_url, supabase_key)
+SUPABASE_URL = "https://ayyogzlsqqbwocmuuwph.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5eW9nemxzcXFid29jbXV1d3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MjA5NTEsImV4cCI6MjAzMjE5Njk1MX0.9y9t591c8v1RNCssBa4-GNK9ZQNpR7CDqVJfvycnWCk"
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+
+@app.route('/api/profile', methods=['GET'])
+def get_data():
+    try:
+        user_id = request.args.get('user_id')
+        if not user_id:
+            return jsonify({"error": "user_id is required"}), 400
+
+        response = supabase \
+            .from_('profiles') \
+            .select('*') \
+            .eq('id', user_id) \
+            .execute()
+        data = response.data
+
+        return jsonify(data[0]), 200
+
+    except Exception as error:
+        print(f"Error fetching user profile: {error}")
+        return jsonify({"error": str(error)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
