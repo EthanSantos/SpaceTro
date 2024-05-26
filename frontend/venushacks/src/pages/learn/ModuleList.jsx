ce63c1faef7dd626a59a98421d6f7d43cf4a9f6d
Riankai1073 <riancorci@gmail.com>
2024-05-26 02:23:49 -0700

    merging

--- frontend/venushacks/src/pages/learn/ModuleList.jsx
+++ frontend/venushacks/src/pages/learn/ModuleList.jsx
@@ -23,6 +23,36 @@ const ModuleList = () => {
                     <p>Module 3</p>
                     <h3>Jupiter</h3>
                 </Link>
+
+                <Link to="/module" className='module'>
+                    <img src="" alt="planet" />
+                    <p>Module 4</p>
+                    <h3>Earth</h3>
+                </Link>
+
+                <Link to="/module" className='module'>
+                    <img src="" alt="planet" />
+                    <p>Module 5</p>
+                    <h3>Pluto</h3>
+                </Link>
+
+                <Link to="/module" className='module'>
+                    <img src="" alt="planet" />
+                    <p>Module 6</p>
+                    <h3>Saturn</h3>
+                </Link>
+
+                <Link to="/module" className='module'>
+                    <img src="" alt="planet" />
+                    <p>Module 7</p>
+                    <h3>Uranus</h3>
+                </Link>
+
+                <Link to="/module" className='module'>
+                    <img src="" alt="planet" />
+                    <p>Module 8</p>
+                    <h3>Neptune</h3>
+                </Link>
             </div>
         </div>
     );
